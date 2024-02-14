03b3e7138bee841b00c3b2ec806bf884b2457921
Delaynie <delaynieym@outlook.com>
2024-02-14 00:37:06 -0500

    added docstrings for most of the dependencies

--- steering_teensy/src/rc.cpp
+++ steering_teensy/src/rc.cpp
@@ -3,13 +3,19 @@
 #include <Arduino.h>
 #include <ArduinoCRSF.h>
 
-namespace rc {
+/**
+ * @brief Wrapper namespace for the TX12 rc controller.
+ * Communication to hte controller is implemented using the ArduinoCRSF class.
+ *
+ */
+namespace rc
+{
 
 #define RC_SERIAL Serial6
 #define RC_BAUDRATE 115200
 
-#define CHANNEL_LEFT_X  4
-#define CHANNEL_LEFT_Y  3
+#define CHANNEL_LEFT_X 4
+#define CHANNEL_LEFT_Y 3
 #define CHANNEL_RIGHT_X 1
 #define CHANNEL_RIGHT_Y 2
 #define CHANNEL_SWITCH_E 5
@@ -19,57 +25,105 @@ namespace rc {
 #define CHANNEL_BUTTON_A 9
 #define CHANNEL_BUTTON_D 10
 
-ArduinoCRSF rc_controller;
+	ArduinoCRSF rc_controller;
 
-void init() {
-	RC_SERIAL.begin(115200);
-	if (!RC_SERIAL) {
-		while (1) {
-			Serial.println("RC serial port init failed!");
-			delay(1000);
+	/**
+	 * @brief Initializes hardware.  Should be called in the main setup() function.
+	 */
+	void init()
+	{
+		RC_SERIAL.begin(115200);
+		if (!RC_SERIAL)
+		{
+			while (1)
+			{
+				Serial.println("RC serial port init failed!");
+				delay(1000);
+			}
 		}
+		rc_controller.begin(RC_SERIAL);
 	}
-	rc_controller.begin(RC_SERIAL);
-}
 
-void update() {
-	rc_controller.update();
-}
+	/**
+	 * @brief Calls the controller's internal update function.
+	 *
+	 */
+	void update()
+	{
+		rc_controller.update();
+	}
+	/**
+	 * @brief Calls the controller's internal getLinkStatistics function.
+	 *
+	 * @return const crsfLinkStatistics_t& A pointer to a struct containing statistics of the connection to the controller.
+	 */
+	const crsfLinkStatistics_t &link_statistics()
+	{
+		return *rc_controller.getLinkStatistics();
+	}
 
-const crsfLinkStatistics_t& link_statistics() {
-	return *rc_controller.getLinkStatistics();
-}
-
-bool connected() {
-	return rc_controller.isLinkUp();
-}
+	/**
+	 * @return true The rc controler is connected to the buggy.
+	 * @return false The rc controller is not connected to the buggy.
+	 */
+	bool connected()
+	{
+		return rc_controller.isLinkUp();
+	}
 
 #define RC_STEERING_DEGREES 30.0
 
-bool operator_ready() {
-	if (connected()) {
-		return (rc_controller.getChannel(CHANNEL_BUTTON_A) > 1500) || (rc_controller.getChannel(CHANNEL_BUTTON_D) > 1500);
-	} else {
-		return false;
+	/**
+	 * @brief Effectively, this serves to check that the human holding the rc controller is ready for the buggy to be moving.
+	 * This function checks that the A or D button on the controller is held down.
+	 *
+	 * @return true The person holding the controller is ready for the buggy to start moving.
+	 * @return false The person holding the controller is not ready for the buggy to start moving.
+	 */
+	bool operator_ready()
+	{
+		if (connected())
+		{
+			return (rc_controller.getChannel(CHANNEL_BUTTON_A) > 1500) || (rc_controller.getChannel(CHANNEL_BUTTON_D) > 1500);
+		}
+		else
+		{
+			return false;
+		}
 	}
-}
 
-float steering_angle() {
-	if (connected()) {
-		int raw_width = rc_controller.getChannel(CHANNEL_RIGHT_X);
+	/**
+	 * @brief Returns the angle that the steering wheel is pointing to.
+	 * Defaults to zero if there is no connection to the controller.
+	 *
+	 * @return float Units are in degrees.  Zero degrees is straight forward.
+	 * Positive values are left of center.  Negative values are right of center.
+	 * In other words, CCW is positive.
+	 */
+	float steering_angle()
+	{
+		if (connected())
+		{
+			int raw_width = rc_controller.getChannel(CHANNEL_RIGHT_X);
 
-		// Scaled to -1.0 to 1.0, left positive
-		float analog = -1.0 * (raw_width - 1500.0) / 500.0;
+			// Scaled to -1.0 to 1.0, left positive
+			float analog = -1.0 * (raw_width - 1500.0) / 500.0;
 
-		return analog * RC_STEERING_DEGREES;
-	} else {
-		return 0.0;
+			return analog * RC_STEERING_DEGREES;
+		}
+		else
+		{
+			return 0.0;
+		}
 	}
-}
 
-bool use_autonomous_steering() {
-	bool auto_switch = (rc_controller.getChannel(CHANNEL_SWITCH_E) > 1750);
-	return operator_ready() && auto_switch;
-}
+	/**
+	 * @brief Checks if the "use autonomous steering" switch on the controller is on or not.
+	 */
+	bool use_autonomous_steering()
+	{
+		bool auto_switch = (rc_controller.getChannel(CHANNEL_SWITCH_E) > 1750);
+		return operator_ready() && auto_switch;
+	}
 
 } // namespace rc

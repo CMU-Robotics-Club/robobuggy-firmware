ccfb12f125527a90f6db74a407c30598e339cc75
Delaynie <delaynieym@outlook.com>
2024-11-13 18:05:14 -0500

    re-added circular array differencing for NAND's encoder

--- nand_teensy/include/encoder.h
+++ nand_teensy/include/encoder.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 namespace encoder {
 
 void init();
@@ -8,14 +10,13 @@ void update();
 
 int steps();
 
+uint16_t e_raw_angle();
+uint16_t e_angle();
+bool e_m_strong();
+bool e_m_weak();
+
 double front_speed();
 
-/**
- * @brief Calculates the speed of the buggy's rear, given the angle at which it's steering
- * (and implicitly the speed at which the front is moving)
- * 
- * @param steering_angle Steering angle IN DEGREES.
- */
 double rear_speed(double steering_angle);
 
 }

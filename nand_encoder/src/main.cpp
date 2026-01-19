d18d8e8a2954959ba2e7fb2e8f8d2c9c3d079a71
Anna Delale-O'Connor <anna.delaleoconnor@gmail.com>
2026-01-19 16:32:54 -0500

    Modified encoder for data transfer to teensy

--- nand_encoder/src/main.cpp
+++ nand_encoder/src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <cstdint>
+#include <array>
+#include <cstring>
 #include "encoder.h"
 #include <HardwareSerial.h>
 
@@ -6,6 +9,19 @@
 #define SERIAL_PRINT_PERIOD_MS 100
 
 #define FILTER_BUFFER_SIZE 128
+
+// Packet networking
+#define COMM_SERIAL Serial1
+#define COMM_BAUDRATE 115200
+#define SYNC_LEN 4
+
+const std::array<uint8_t, SYNC_LEN> SYNC_WORD = {0xAA, 0xFF, 0x00, 0x55};
+
+enum class EncoderMessageHeader : uint8_t
+{
+  ErrorInfo = 'E',
+  SpeedInfo = 'S'
+} header;
 
 class RateLimit
 {
@@ -41,17 +57,30 @@ RateLimit enc_read_limit{ENCODER_READ_PERIOD_MS};
 RateLimit serial_send_limit{SERIAL_PRINT_PERIOD_MS};
 
 float speed;
+float filtered_speed;
 float speed_filter[FILTER_BUFFER_SIZE];
 
 int filter_idx;
 float speed_filter_sum;
 
+// Packet networking
+void write_speed_packet()
+{
+  COMM_SERIAL.write(SYNC_WORD.data(), SYNC_LEN); // Sync word
+
+  filtered_speed = speed_filter_sum / FILTER_BUFFER_SIZE;
+  header = EncoderMessageHeader::SpeedInfo;
+  COMM_SERIAL.write(header); // Message type
+  byte* bytes = (byte*)&filtered_speed;
+  COMM_SERIAL.write(bytes, sizeof(filtered_speed)); // Message
+}
+
 void setup()
 {
-  //pinMode(3, OUTPUT);
-  //digitalWrite(3, HIGH);
+  // pinMode(3, OUTPUT);
+  // digitalWrite(3, HIGH);
   Serial.begin(115200);
-  Serial1.begin(115200);
+  COMM_SERIAL.begin(COMM_BAUDRATE);
   Serial.print("init enc");
   encoder::init();
   filter_idx = 0;
@@ -60,8 +89,8 @@ void setup()
 
 void loop()
 {
-  //Serial1.println("encoder ready");
-  //Serial.println("we're alive");
+  // Serial1.println("encoder ready");
+  // Serial.println("we're alive");
   if (enc_read_limit.ready())
   {
     speed = encoder::get_degrees_per_second();
@@ -78,13 +107,18 @@ void loop()
     Serial.print("\tencoder speed: ");
     Serial.print(speed);
     Serial.print("\tfiltered speed: ");
-    Serial.println(speed_filter_sum/FILTER_BUFFER_SIZE);
+    Serial.println(speed_filter_sum / FILTER_BUFFER_SIZE);
 
+    write_speed_packet();
+    // Add error packet later
+
+    /*
     Serial1.print("encoder pos: ");
     Serial1.print(encoder::get_degrees());
     Serial1.print("\tencoder speed: ");
     Serial1.print(speed);
     Serial1.print("\tfiltered speed: ");
-    Serial1.println(speed_filter_sum/FILTER_BUFFER_SIZE);
+    Serial1.println(speed_filter_sum / FILTER_BUFFER_SIZE);
+    */
   }
 }
